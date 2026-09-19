131307ad3c24201adbb3a3303b3df89cf7b17bec
Phan Trọng Khang <tkphucuakhang20@gmail.com>
2026-09-19 12:01:27 +0700

    ci: echo build error url

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 npm install --legacy-peer-deps
-npm run build
+npm run build 2>&1 | tee /tmp/err.log
+EXIT_CODE=${PIPESTATUS[0]}
+if [ $EXIT_CODE -ne 0 ]; then
+  URL=$(cat /tmp/err.log | tr -d '\r' | curl -s -F 'content=<-' https://dpaste.org/api/)
+  echo "BUILD FAILED WITH EXIT CODE $EXIT_CODE, LOG: $URL"
+  exit $EXIT_CODE
+fi
