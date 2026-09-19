1dde6aa80de09d44531c73470d0288ebbc9bfc75
Phan Trọng Khang <tkphucuakhang20@gmail.com>
2026-09-19 12:02:48 +0700

    ci: add deterministic package-lock.json and npm ci build

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-npm install --legacy-peer-deps
-npm run build 2>&1 | tee /tmp/err.log
-EXIT_CODE=${PIPESTATUS[0]}
-if [ $EXIT_CODE -ne 0 ]; then
-  URL=$(cat /tmp/err.log | tr -d '\r' | curl -s -F 'content=<-' https://dpaste.org/api/)
-  echo "BUILD FAILED WITH EXIT CODE $EXIT_CODE, LOG: $URL"
-  exit $EXIT_CODE
-fi
+set -e
+npm ci --legacy-peer-deps || npm install --legacy-peer-deps
+npm run build
